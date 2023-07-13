5019e47cc168bdd261da49357470e255a797e7d0
Julian <T112.P05A@stud.provadis-hochschule.de>
2023-07-13 14:14:18 +0200

    Überarbeitung, Spiel erstellen Code + Beitrittscode ( noch in arbeit)

--- Pages/overview.cshtml.cs
+++ Pages/overview.cshtml.cs
@@ -1,12 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace BlackJack.Pages
 {
     public class Index1Model : PageModel
     {
-        public void OnGet()
+        public string? code { get; set; }
+        public string? GameID { get; set; }
+        private readonly SqlConnection conn = new("Server=provadis-it-ausbildung.de;Database=BlackJack02;UID=BlackJackUser02;PWD=Pr@vadis_188_Pta;");
+        private SqlCommand? cmd;
+        private SqlDataReader? reader;
+        private readonly SqlDataAdapter? adapter = new SqlDataAdapter();
+        
+        public void OnPost()
         {
+            int.Parse(code);
+            int.Parse(GameID);
+
+            
+        }
+        private Random randomZahl()
+        {
+            var rnd = new Random();
+            rnd.Next();
+            return rnd;
+        }
+        
+        private int additionGameID()
+        {
+            
+            return 
         }
     }
 }
+
+    
