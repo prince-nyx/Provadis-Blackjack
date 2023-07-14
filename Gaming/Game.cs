97479a2f5aa1468346f50b65c6e5197360322c5e
Lewnox <lewnox@gmail.com>
2023-07-14 15:14:52 +0200

    update

--- Gaming/Game.cs
+++ Gaming/Game.cs
@@ -11,18 +11,19 @@ public class Game
     public String id { get; }
     private CardDeck deck;
     private String[] slots = new String[7];
-    private String host;
+    public String hostid { get; set; }
     private Dictionary<string, Player> players = new Dictionary<string, Player>(); 
     private CardDeck dealerDeck;
+    public GameHub hub { get; set; }
 
-    private int currentSlotsTurn = 0;
+    private int currentSlotsTurn = -1;
 
 
-    public Game()
+    public Game(String hostid)
     {
-        host = string.Empty;
+        this.hostid = hostid;
         this.id = Program.GenerateRandomString(4).ToUpper();
-        Console.WriteLine("Game erstellt mit Code "+id);
+        Console.WriteLine("Game erstellt mit Code "+id+" und host "+hostid);
         dealerDeck = new CardDeck();
         deck = new CardDeck();
         deck.createBlackJackDeck();
@@ -32,20 +33,38 @@ public class Game
     }
 
 
-    public void start()
+    public void startGame()
     {
-
+        currentSlotsTurn = -1;
+        Console.WriteLine("SPIEL GESTARTET");
+        dealCard(true);
+        dealCard(false);
+        nextPlayer();
     }
 
-    public void dealCard()
+    public void nextPlayer()
+    {
+        currentSlotsTurn++;
+        Console.WriteLine("Start turn");
+        if (slots[currentSlotsTurn] != null && players.ContainsKey(slots[currentSlotsTurn]))
+        {
+            _ = getHub().startTurn(players[slots[currentSlotsTurn]], 20);
+        }
+        else
+            nextPlayer();
+    }
+
+    public void dealCard(Boolean hidden)
     {
         Card card = deck.drawCard();
         dealerDeck.addCard(card);
+        _ = getHub().addDealerCard(card.getName(),hidden);
+
         for (int i = 0; i < slots.Length; i++)
         {
-            Player player = players[slots[i]];
-            if(player != null)
+            if(slots[i] != null && players.ContainsKey(slots[i]))
             {
+                Player player = players[slots[i]];
                 card = deck.drawCard();
                 player.addCard(card);
                 _ = getHub().addCardToPlayer(i, card.getName());
@@ -63,8 +82,7 @@ public class Game
             {
                 slots[i] = player.id;
                 players.Add(player.id, player);
-                if (players.Count == 1)
-                    host = player.id;
+                _ = getHub().assignPlayer(i, player.username);
                 break;
             }
         }
@@ -81,10 +99,9 @@ public class Game
         return "Game(id:" + id + " / players:" + players.Count + ")";
     }
 
-
     public GameHub getHub()
     {
-        return players[host].hub;
+        return hub;
     }
     
     public void hit(int slotid)
@@ -99,9 +116,5 @@ public class Game
             getHub().addCardToPlayer(slotid,card.getName()); 
            }
         
-    }
-    public void startGame()
-    {
-
     }
 }
--- Hubs/GameHub.cs
+++ Hubs/GameHub.cs
@@ -26,66 +26,123 @@ namespace BlackJack.Hubs
                 await Clients.All.SendAsync("console", "Kein Login vorhanden");
             else
             {
-                player.hub = this;
+                //player.hub = this;
                 player.connectionId = Context.ConnectionId;
-                Console.WriteLine($"{player.connectionId} Connection ID");
+                Game game = Program.app.gameManager.getGame(player.currentGameId);
+                if (game != null && game.hostid.Equals(player.id))
+                {
+                    game.hub = this;
+                    game.addPlayer(player);
+                }
                 await Clients.All.SendAsync("console", "Eingeloggt als " + player.username);
 
             }
         }
 
+        public async Task startGame(String cookie)
+        {
+            Player player = Program.app.playerManager.getPlayer(cookie);
+            if (player == null)
+                await Clients.All.SendAsync("console", "Kein Login vorhanden");
+            else
+            {
+                Game game = Program.app.gameManager.getGame(player.currentGameId);
+                if(game == null)        
+                    await Clients.All.SendAsync("console", "Game nicht gefunden");
+                else
+                {
+                    game.startGame();
+                    await Clients.All.SendAsync("console", "Game gestartet");
+                }
+            }
+        }
+
+
+        //test
+        public async Task testConnection(String message)
+        {
+            await Clients.All.SendAsync("console", message);
+
+        }
+
+        //all
         public async Task addCardToPlayer(int slotid, String cardname)
         {
             await Clients.All.SendAsync("console", "[EVENT] addCardToPlayer("+slotid +" "+cardname+")");
         }
 
+        //all
         public async Task setCardSum(int slotid, int amount)
         {
             await Clients.All.SendAsync("console", "[EVENT] setCardSum(" + slotid + " " + amount + ")");
         }
+
+        //all
         public async Task assignPlayer(int slotid, String username)
         {
             await Clients.All.SendAsync("console", "[EVENT] assignPlayer(" + slotid + " " + username + ")");
         }
+
+        //all
         public async Task unassignPlayer(int slotid)
         {
             await Clients.All.SendAsync("console", "[EVENT] unassignPlayer(" + slotid + ")");
         }
-        public async Task endTurn(int slotid)
-        {
-            await Clients.All.SendAsync("console", "[EVENT] endTurn(" + slotid + ")");       
-        }
-        public async Task startTurn(int slotid, int time)
-        {
-            await Clients.All.SendAsync("console", "[EVENT] startTurn(" + slotid + " " + time + ")");
-        }
-        public async Task setbBalance(Player player, int amount)
-        {
-            await Clients.All.SendAsync("console", "[EVENT] setbBalance(" + amount + " )");
-        }
-        public async Task enableBet(Player player)
-        {
-            await Clients.All.SendAsync("console", "[EVENT] enableBet()");
-        }
-        public async Task disableBet(Player player)
-        {
-            await Clients.All.SendAsync("console", "[EVENT] disableBet()");
-        }
+
+        //all
         public async Task setBet(int slotid, double amount)
         {
             await Clients.All.SendAsync("console", "[EVENT] setBet(" + amount + ")");
         }
+
+        //all
+        public async Task addDealerCard(String cardname, Boolean hidden)
+        {
+            Console.WriteLine("[EVENT] addDealerCard(" + cardname + ", " + hidden + ")");
+            await Clients.All.SendAsync("console", "[EVENT] addDealerCard(" + cardname + ", " + hidden + ")");
+        }
+
+        //client
+        public async Task endTurn(Player player, int slotid)
+        {
+            await Clients.Client(player.connectionId).SendAsync("console", "[EVENT] endTurn(" + slotid + ")");       
+        }
+
+        //client
+        public async Task startTurn(Player player, int time)
+        {
+            await Clients.Client(player.connectionId).SendAsync("console", "[EVENT] startTurn(" + time + ")");
+        }
+
+        //client
+        public async Task setbBalance(Player player, int amount)
+        {
+            await Clients.Client(player.connectionId).SendAsync("console", "[EVENT] setbBalance(" + amount + " )");
+        }
+
+        //client
+        public async Task enableBet(Player player)
+        {
+            await Clients.Client(player.connectionId).SendAsync("console", "[EVENT] enableBet()");
+        }
+
+        //client
+        public async Task disableBet(Player player)
+        {
+            await Clients.Client(player.connectionId).SendAsync("console", "[EVENT] disableBet()");
+        }
+
+
+        //client
         public async Task showResult(Player player, int amount, String result)
         {
-            await Clients.All.SendAsync("console", "[EVENT] showResult(" + amount + " " + result + ")");
+            await Clients.Client(player.connectionId).SendAsync("console", "[EVENT] showResult(" + amount + " " + result + ")");
         }
-        public async Task addDealerCard(String cardname)
-        {
-            await Clients.All.SendAsync("console", "[EVENT] addDealerCard(" + cardname + ")");
-        }
+
+        //client
         public async Task showStartButton(Player player)
         {
-            await Clients.All.SendAsync("console", "[EVENT] showStartButton()");
+            await Clients.Client(player.connectionId).SendAsync("console", "[EVENT] showStartButton()");
         }
     }
 }
