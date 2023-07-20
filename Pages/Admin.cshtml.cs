cc7f540f2422abcbfce6d781b73573448eb6b569
Julian <T112.P05A@stud.provadis-hochschule.de>
2023-07-20 13:32:37 +0200

    Adminsettings Done

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,39 +1,69 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-<<<<<<< HEAD
-=======
+using System.Data.SqlClient;
+
 using System.Reflection.PortableExecutable;
 using System.Data.SqlClient;
 
->>>>>>> 33ded14887aa561d866c9cf4fc73df7f3d1a7e7b
+
 namespace BlackJack.Pages
 {
     public class AdminModel : PageModel
     {
-<<<<<<< HEAD
+
         public int betTime { get; set; }
         public int turnTime { get; set; }
         public double potLimit { get; set; }
         public double maxEinzahlung { get; set; }
         public double startguthaben { get; set; }
 
+        private SqlCommand? cmd;
+        private SqlDataReader? reader;
+        private readonly SqlDataAdapter? adapter = new SqlDataAdapter();
+        private readonly SqlConnection conn = new("Server=provadis-it-ausbildung.de;Database=BlackJack02;UID=BlackJackUser02;PWD=Pr@vadis_188_Pta;");
+
         public void OnGet()
         {
-            //START ACCESS CHECK
+            Boolean isAdmin = false;
             String userid = Request.Cookies["userid"];
             String result = Program.app.checkAccess(userid);
-            if (result.Equals("logout"))
+            if (result.Equals("ingame") || result.Equals("loggedin"))
             {
-                Response.Cookies.Delete("userid");
+                Player player = Program.app.playerManager.getPlayer(userid);
+                if (player != null)
+                {
+                    string sql = "SELECT DISTINCT isAdmin FROM Benutzer WHERE Username = @Username";
+
+                    this.conn.Open();
+                    this.cmd = new SqlCommand(sql, this.conn);
+                    this.cmd.Parameters.AddWithValue("@Username", player.username);
+                    this.reader = this.cmd.ExecuteReader();
+                    while (this.reader.Read())
+                    {
+                        isAdmin = reader.GetBoolean(0);
+
+                    }
+                    this.reader.Close();
+                    this.cmd.Dispose();
+                    this.conn.Close();
+                }
+                if (!isAdmin)
+                {
+                    if (result.Equals("ingame"))
+                        Response.Redirect("/game");
+                    else
+                        Response.Redirect("/overview");
+                }
             }
-            else
+            else if (result.Equals("logout"))
             {
-                Response.Redirect("/overview");
+                Response.Redirect("/index");
             }
-            //END ACCESS CHECK
+
         }
         public void OnPost()
         {
+            Console.WriteLine("Methode OnPost() wurde aufgerufen");
             this.betTime = Convert.ToInt32(Request.Form["betTime"]);
             this.turnTime = Convert.ToInt32(Request.Form["turnTime"]);
             this.potLimit = Convert.ToDouble(Request.Form["potLimit"]);
@@ -58,49 +88,8 @@ namespace BlackJack.Pages
             Console.WriteLine("Vor Reload");
                 Program.app.settings.reload();
             Console.WriteLine("Reload");
-=======
-        private SqlCommand? cmd;
-        private SqlDataReader? reader;
-        private readonly SqlDataAdapter? adapter = new SqlDataAdapter();
-        private readonly SqlConnection conn = new("Server=provadis-it-ausbildung.de;Database=BlackJack02;UID=BlackJackUser02;PWD=Pr@vadis_188_Pta;");
-        public void OnGet()
-        {
-            Boolean isAdmin = false;
-            String userid = Request.Cookies["userid"];
-            String result = Program.app.checkAccess(userid);
-            if (result.Equals("ingame") || result.Equals("loggedin"))
-            {
-                Player player = Program.app.playerManager.getPlayer(userid);
-                if(player != null)
-                {
-                    string sql = "SELECT DISTINCT isAdmin FROM Benutzer WHERE Username = @Username";
 
-                    this.conn.Open();
-                    this.cmd = new SqlCommand(sql, this.conn);
-                    this.cmd.Parameters.AddWithValue("@Username", player.username);
-                    this.reader = this.cmd.ExecuteReader();
-                    while (this.reader.Read())
-                    {
-                        isAdmin = reader.GetBoolean(0);
 
-                    }
-                    this.reader.Close();
-                    this.cmd.Dispose();
-                    this.conn.Close();
-                }
-                if(!isAdmin)
-                {
-                    if(result.Equals("ingame"))
-                        Response.Redirect("/game");
-                    else
-                        Response.Redirect("/overview");
-                }
-            }
-            else if (result.Equals("logout"))
-            {
-                Response.Redirect("/index");
-            }
->>>>>>> 33ded14887aa561d866c9cf4fc73df7f3d1a7e7b
         }
     }
 }
